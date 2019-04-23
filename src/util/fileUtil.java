5596fc0c9e1b07c97c304f4148fcc40b25f435ae
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2019-04-23 19:55:52 +0300

    Added createGrid() and modified readText() so if there is any blank lines in text it wont throw exception (happened in level2)

--- src/util/fileUtil.java
+++ src/util/fileUtil.java
@@ -10,18 +10,46 @@ import java.util.Scanner;
 public class fileUtil {
 
 
+    /*
+    So readText() method basically reads the text and gives us a 1 dimensional
+    array list we want to convert that to a 2D 4x4 grid
+     */
+    public static Tile[][] createGrid(int level){
+
+        int pos = 0;
+        Tile[][] grid = new Tile[4][4];
+        ArrayList<Tile> tiles = readText(level);
+
+
+        for(int row=0;row<4;row++){
+            for(int col=0;col<4;col++){
+
+                grid[row][col] = tiles.get(pos);
+                pos++;
+
+            }
+        }
+
+        return grid;
+
+    }
+
+
     public static ArrayList<Tile> readText(int level) { // Reading text with level parameter
 
         ArrayList<Tile> tiles = new ArrayList<>();
 
         try {
 
-            File file = new File("levels/level" + level + ".txt");
+            File file = new File("level" + level + ".txt");
             Scanner scanner = new Scanner(file);
 
             while (scanner.hasNext()) {
 
                 String line = scanner.nextLine(); // id,type,property
+                if(line.trim().equalsIgnoreCase("")){
+                    continue;
+                }
                 String[] lineContents = line.split(","); // [id, type , property]
                 int position = Integer.parseInt(lineContents[0]);
                 int[] coordinates = calculateCoordinatesfromPos(position);
@@ -58,7 +86,7 @@ public class fileUtil {
                         break;
 
 
-                    case "EmptyTile":
+                    case "Empty":
 
                         if(direction.equalsIgnoreCase("Free")){
 
@@ -114,6 +142,7 @@ public class fileUtil {
         }
         return tiles;
     }
+
 
     // Calculate x and y values from position (Ex : 5 > x: 1 y:1)
     public static int[] calculateCoordinatesfromPos(int position) {
