71ef9e8ee1764beb37516b6e3ae2e62846161777
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2019-04-23 21:21:29 +0300

    now showGrid() actually uses the input file

--- src/Gui.java
+++ src/Gui.java
@@ -1,39 +1,26 @@
-import game.EmptyTile;
 import game.Tile;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-
-import java.util.ArrayList;
-import java.util.Random;
+import javafx.stage.Stage;
 
 public class Gui {
 
-
     private Tile[][] tiles;
     private GridPane pane;
-    private final int SIZE = 430;
+    private static final int SIZE = 430;
     private Scene scene;
 
 
-    public Gui(Tile[][] tiles) {
+    public Gui(Tile[][] tiles) { // Constructor of GUI
 
-        this.tiles = tiles;
+        this.tiles = tiles; // Get the files from argument which gives the input read from file
         pane = new GridPane();
         for (int row = 0; row < tiles.length; row++) {
             for (int col = 0; col < tiles[row].length; col++) {
 
-
-                pane.add(tiles[row][col], row, col);
-
-
+                pane.add(tiles[row][col],col ,row ); // iterate through the matrix add all the tiles to the pane
             }
-
 
         }
 
@@ -41,36 +28,37 @@ public class Gui {
     }
 
 
-    public void showGrid(){
+    public void showGrid(Stage stage){
 
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(5,5,5,5));
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
 
-        Random random = new Random();
-        scene = new Scene(gridPane, SIZE, SIZE);
+        pane.setPadding(new Insets(3,3,3,3));
+        pane.setHgap(3);
+        pane.setVgap(3);
+
+        scene = new Scene(pane, SIZE, SIZE); // Creating scene with pane
+
 
         for(int row=0;row<tiles.length;row++){
             for(int col=0;col<tiles[row].length;col++){
 
-                EmptyTile tile = new EmptyTile(row,col);
+                Tile tile = tiles[row][col];
 
-                tile.setStroke(Color.BLACK);
-               tile.setFill(Color.rgb(random.nextInt(255),random.nextInt(255) ,random.nextInt(255) ));
+                tile.setOnMouseClicked(e -> { // Event handler
 
-                gridPane.add(tile,row,col ); // CHECKED THIS IS CORRECT.
-                tile.setOnMouseClicked(e -> {
-
-                    EmptyTile node = (EmptyTile) e.getPickResult().getIntersectedNode();
-                    System.out.println(node.getxGrid()+" , " + node.getyGrid());
+                    Tile t = (Tile) e.getPickResult().getIntersectedNode();
+                    System.out.println(t.getxGrid()+" , " + t.getyGrid()+" ,"+t.getClass().getName());
 
                 });
+
+                // Bind the tiles to the screen to make the page responsive
                 tile.widthProperty().bind(scene.widthProperty().divide(4).subtract(7));
                 tile.heightProperty().bind(scene.heightProperty().divide(4).subtract(7));
 
             }
         }
+
+        stage.setScene(scene);
+        stage.show();
 
     }
 
