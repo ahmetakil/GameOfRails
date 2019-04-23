631241ae54b2c5e7549f36fc10fa6b0f48bee26b
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2019-04-23 19:54:53 +0300

    Now Tile class extends Rectangle to being able to use all the rectangle methods like setStoke() etc. but we cannot use regular x, y which is used for width and height

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -1,34 +1,21 @@
 package game;
 
 import javafx.scene.image.Image;
+import javafx.scene.shape.Rectangle;
 
-public abstract class Tile {
-    private int x; // Location of the tile at x-axis in the grid pane
-    private int y; // Location of the tile at y-axis in the grid pane
+public abstract class Tile extends Rectangle {
+    private int xGrid; // Location of the tile at x-axis in the grid pane
+    private int yGrid; // Location of the tile at y-axis in the grid pane
     private Image image;
     private boolean movable; //A boolean variable that keeps is tile static or not.
 
     public Tile(int x, int y, boolean movable) { //Construction of the object
-        this.x = x;
-        this.y = y;
+        this.xGrid = x;
+        this.yGrid = y;
         this.movable = movable;
     }
 
-    public int getX() { //Getter Setter methods.
-        return x;
-    }
 
-    public void setX(byte x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(byte y) {
-        this.y = y;
-    }
 
     public void setImage(Image image) {
         this.image = image;
@@ -38,4 +25,12 @@ public abstract class Tile {
         return movable;
     }
 
+
+    public int getxGrid() {
+        return xGrid;
+    }
+
+    public int getyGrid() {
+        return yGrid;
+    }
 }
